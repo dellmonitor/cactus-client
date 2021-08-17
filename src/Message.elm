2e0502f91c645f93a8b7555c824bd1d51bb8fea9
asdfkcdk <9508901-asdfkcdk@users.noreply.gitlab.com>
2021-08-17 14:10:45 +0300

    use cactus-comment-displayname on anchor directly without paragraph

--- src/Message.elm
+++ src/Message.elm
@@ -217,9 +217,11 @@ viewMessageEvent defaultHomeserverUrl now messageTime senderId sender message =
         , div [ class "cactus-comment-content" ]
             -- name and time
             [ div [ class "cactus-comment-header" ]
-                [ p
-                    [ class "cactus-comment-displayname" ]
-                    [ a [ href matrixDotToUrl ] [ text displayname ] ]
+                [ a
+                    [ class "cactus-comment-displayname"
+                    , href matrixDotToUrl
+                    ]
+                    [ text displayname ]
                 , Accessibility.time
                     [ class "cactus-comment-time", title timeUtc, datetime timeUtcIso ]
                     [ text timeStr ]
